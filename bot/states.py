0b6aca99078a7c937c05c77f31f6e63fa9044bc7
Pomidorchik03 <london-266@mail.ru>
2025-06-07 19:59:08 +0700

    Исправлены ошибки с уведомлением пользователя о новых релизах, добавлены подсказки

--- bot/states.py
+++
@@ -1,9 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class TrackSearch(StatesGroup):
-    waiting_query = State()
-    waiting_track_choice = State()
-
-class ArtistSearch(StatesGroup):
-    waiting_query = State()
-    waiting_artist_choice = State()
